3b2f2343cae5f9a73454d608be58ffe0af6013c2
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-03-20 18:23:08 +0100

    HomePage

---
+++ client/src/components/header.tsx
@@ -0,0 +1,13 @@
+import TeamSphereLogo from "/TeamSphere.svg";
+
+
+export default function Header() {
+    return (
+        <>
+        <div>
+            <img src={TeamSphereLogo} alt="TeamSphere Logo" />
+            Headers
+        </div>
+        </>
+    )
+}
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import Footer from '../components/footer';
+import Header from '../components/header';
+
+function Home() {
+
+  return (
+    <>
+    <Header />
+    <Footer />
+    </>
+  )
+}
+
+export default Home;
