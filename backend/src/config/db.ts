17ef0fac628ed7a162b5f87b7ee8750eb851fa4d
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-03-26 06:41:42 +0300

    mongodb atlas connection is done

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,25 +1,31 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+require("dotenv").config();
 
 class DB {
-    constructor() {
-        const db: string = process.env.DB_CONNECTION || 'mongodb';
-        const host: string = process.env.DB_HOST || 'localhost';
-        const port: string | number = process.env.DB_PORT || 27017;
-        const database: string = process.env.DB_DATABASE || 'TeamSphere';
-        const dbURL: string = `${db}://${host}:${port}/${database}`;
-        mongoose.connect(dbURL, { autoIndex: !process.env.PROUCTION || false })
+  constructor() {
+    // const db: string = process.env.DB_CONNECTION || 'mongodb';
+    // const host: string = process.env.DB_HOST || 'localhost';
+    // const port: string | number = process.env.DB_PORT || 27017;
+    // const database: string = process.env.DB_DATABASE || 'TeamSphere';
+    // const dbURL: string = `${db}://${host}:${port}/${database}`;
+    const uri: string = `${process.env.DB_CONNECTION}`;
+    try {
+      mongoose.connect(uri, { autoIndex: true });
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    async connected(): Promise<boolean | Error> {
-        return new Promise((resolve, reject) => {
-            mongoose.connection.once('open', () => {
-                resolve(true);
-            });
-            mongoose.connection.on('error', (err) => {
-                reject(err);
-            });
-        })
-    }
+  async connected(): Promise<boolean | Error> {
+    return new Promise((resolve, reject) => {
+      mongoose.connection.once("open", () => {
+        resolve(true);
+      });
+      mongoose.connection.on("error", err => {
+        reject(err);
+      });
+    });
+  }
 }
 
 const mongoDB: DB = new DB();
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,22 @@
 import express from "express";
 import DB from "./config/db";
 import redisClient from "./config/redisDB";
+import mongoose from 'mongoose';
+require('dotenv').config()
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
+// const uri: string =`${process.env.DB_URL}`;
+// console.log(uri);
+
+// mongoose.connect(`${process.env.DB_URL}`)
+// .then((result)=>{
+//     console.log('Connected to db');
+//     app.listen(PORT);
+// })
+// .catch((err)=> console.log(err));
+
 
 app.listen(PORT, () => {
   DB.connected()
@@ -15,15 +27,15 @@ app.listen(PORT, () => {
       console.log("DB connection failed", err);
     });
 
-  redisClient
-    .connected()
-    .then((res) => {
-      console.log("Redis connection successful");
-    })
-    .catch((err) => {
-      console.log("Redis connection failed", err);
-    });
+  // redisClient
+  //   .connected()
+  //   .then((res) => {
+  //     console.log("Redis connection successful");
+  //   })
+  //   .catch((err) => {
+  //     console.log("Redis connection failed", err);
+  //   });
   console.log(`Server is running on port ${PORT}`);
-});
+  });
 
 export default app;
