18ec1d958f7502c94fc4326a5bd44ecb0c87520b
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-03-27 12:09:44 +0100

    test

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 
+app.get('/test', (req, res) => {
+  res.json({"hello": "world"})
+})
+
 app.listen(PORT, () => {
   DB.connected()
     .then((res) => {
