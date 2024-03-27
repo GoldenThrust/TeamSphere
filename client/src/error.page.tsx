1a45cf25d69f46e8a2366697c19a0be85573c1ff
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-03-27 13:55:57 +0100

    test deploying

--- client/src/error.page.tsx
+++ client/src/error.page.tsx
@@ -1,11 +1,17 @@
 import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
-    const error = useRouteError();
-    console.error(error);
-    
-    return (
-        <div id="error-page">
+  interface RouteError {
+    statusText: string;
+    message: string;
+  }
+
+  //@ts-ignore
+  const error: RouteError = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
