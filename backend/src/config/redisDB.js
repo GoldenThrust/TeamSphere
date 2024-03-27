7f31616f7ea3defa110e4e9af497409058876d67
mosetf <stanlym315@gmail.com>
2024-03-27 08:19:26 +0300

    Add user model and password hashing

---
+++ backend/src/config/redisDB.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  //add other fields according to the frontend part requirements (odeniji to supply)
+});
+
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
